f91da26697b7beeb5358f3df4fe58d5bfcd256be
supersakana <zacwilliamson@icloud.com>
2022-02-18 17:28:53 -0600

    script logs the first num and next num within an array

--- script.rb
+++ script.rb
@@ -1,3 +1,14 @@
+# ruby script.rb
+
 def bubble_sort(array)
-    puts array.sort
+    i = 0
+    j = 1
+    while j < array.length 
+        puts "#{array[i]} ~ #{array[j]}"
+    
+        i += 1
+        j += 1
+    end
 end
+
+bubble_sort([4,3,78,2,0,2])
