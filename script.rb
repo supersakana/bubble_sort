f17c7517544afcc52c829ac6d9ccfcf1db075144
supersakana <zacwilliamson@icloud.com>
2022-02-19 10:44:17 -0600

    bubble sort is working

--- script.rb
+++ script.rb
@@ -1,15 +1,17 @@
 # ruby script.rb
 
 def bubble_sort(array)
-   array.each do |number|
-    i = 1 
-    while i < array.length
-
-       puts "#{number}  @  #{array[i]}"
-       i += 1
-
+  loop do
+      swapped  = false
+      (array.length - 1).times do |i|
+        if array[i] > array[i + 1]
+            array[i], array[i + 1] = array[i + 1], array[i]
+            swapped = true
         end
     end
+    break if swapped != true
+  end
+  puts array
 end
 
 bubble_sort([4,3,78,2,0,2])
